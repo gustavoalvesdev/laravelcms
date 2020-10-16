0e756846861a97a55fad6c31db1b5625220444ab
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2020-10-16 15:26:06 -0300

    usuarios - lista

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -3,9 +3,36 @@
 @section('title', 'Usuários')
 
 @section('content_header')
-    <h1>Meus Usuários</h1>
+    <h1>
+        Meus Usuários
+        <a href="{{ route('users.create') }}" class="btn btn-sm btn-success">Novo Usuário</a>
+        <!-- btn -->
+    </h1>
 @endsection
 
 @section('content')
-    ...
+    
+    <table class="table table-hover">
+        <tr>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>E-mail</th>
+            <th>Ações</th>
+        </tr>
+        @foreach ($users as $user)
+            <tr>
+                <td>{{ $user->id }}</td>
+                <td>{{ $user->name }}</td>
+                <td>{{ $user->email }}</td>
+                <td>
+                    <a href="{{ route('users.edit', ['user' => $user->id]) }}" class="btn btn-sm btn-info">Editar</a>
+                    <!-- btn -->
+                    <a href="{{ route('users.destroy', ['user' => $user->id]) }}" class="btn btn-sm btn-danger">Excluir</a>
+                    <!-- btn -->
+                </td>
+            </tr>
+        @endforeach
+    </table>
+    <!-- table -->
+
 @endsection
