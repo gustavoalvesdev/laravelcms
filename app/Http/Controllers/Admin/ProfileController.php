e2a92cd78fe81f3eee6acc8066bd9bad1707a53d
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2021-02-14 16:56:17 -0300

    Meu Perfil: Funcionamento

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -3,9 +3,13 @@
 namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
 use App\Http\Controllers\Controller;
 use App\User;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\View\View;
 
 class ProfileController extends Controller
 {
@@ -14,7 +18,11 @@ class ProfileController extends Controller
         $this->middleware('auth');
     }
 
-    public function index()
+
+    /**
+     * @return Illuminate\View\View
+     */
+    public function index() : View
     {
 
         // logged in user
@@ -28,5 +36,82 @@ class ProfileController extends Controller
 
         return redirect()->route('admin');
 
+    }
+
+    /**
+     * Save the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return Illuminate\Http\RedirectResponse
+     */
+    public function save(Request $request) : RedirectResponse
+    {
+        // logged in user
+        $loggedId = intval(Auth::id());
+
+        $user = User::find($loggedId);
+
+        if ($user) {
+
+            $data = $request->only([
+                'name',
+                'email',
+                'password',
+                'password_confirmation'
+            ]);
+
+            $validator = Validator::make([
+                'name' => $data['name'],
+                'email' => $data['email']
+            ], [
+                'name' => ['required', 'string', 'max:100'],
+                'email' => ['required', 'string', 'email', 'max:100']
+            ]);
+
+            $user->name = $data['name'];
+
+            if ($user->email != $data['email']) {
+                $hasEmail = User::where('email', $data['email'])->get();
+                if (count($hasEmail) === 0) {
+                    $user->email = $data['email'];
+                } else {
+                    $validator->errors()->add('email', __('validation.unique', [
+                        'attribute' => 'email'
+                    ]));
+                }
+            }
+
+            if (!empty($data['password'])) {
+
+                if (strlen($data['password']) >= 4) {
+                    if ($data['password'] === $data['password_confirmation']) {
+                        $user->password = Hash::make($data['password']);
+                    } else {
+                        $validator->errors()->add('password', __('validation.confirmed', [
+                            'attribute' => 'password'
+                        ]));
+                    }
+                } else {
+                    $validator->errors()->add('password', __('validation.min.string', [
+                        'attribute' => 'password',
+                        'min' => 4
+                    ]));
+                }
+
+            }
+
+            if (count($validator->errors()) > 0) {
+                return redirect()->route('profile', [
+                    'user' => $loggedId
+                ])->withErrors($validator);
+            }
+
+            $user->save();
+
+            return redirect()->route('profile')
+            ->with('warning', 'Informações alteradas com sucesso!');
+        }
+
+        return redirect()->route('profile');
     }
 }
