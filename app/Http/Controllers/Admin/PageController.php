2aaef807e4a0b0e653c32c10c78756e3465eca0f
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2021-02-16 15:01:32 -0300

    Página: Editar

--- app/Http/Controllers/Admin/PageController.php
+++ app/Http/Controllers/Admin/PageController.php
@@ -97,7 +97,15 @@ class PageController extends Controller
      */
     public function edit($id)
     {
-        //
+        $page = Page::find($id);
+
+        if ($page) {
+            return view('admin.pages.edit', [
+                'page' => $page
+            ]);
+        }
+
+        return redirect()->route('page.index');
     }
 
     /**
@@ -109,7 +117,51 @@ class PageController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $page = Page::find($id);
+
+        if ($page) {
+
+            $data = $request->only([
+                'title',
+                'body'
+            ]);
+
+            if ($page['title'] !== $data['title']) {
+
+                $data['slug'] = Str::slug($data['title'], '-');
+
+                $validator = Validator::make($data, [
+                    'title' => ['required', 'string', 'max:100'],
+                    'body' => ['string'],
+                    'slug' => ['required', 'string', 'max:100', 'unique:pages']
+                ]);
+
+            } else {
+                $validator = Validator::make($data, [
+                    'title' => ['required', 'string', 'max:100'],
+                    'body' => ['string']
+                ]);
+            }
+
+            if ($validator->fails()) {
+                return redirect()->route('pages.edit', [
+                    'page' => $id
+                ])
+                ->withErrors($validator)
+                ->withInput();
+            }
+
+            $page->title = $data['title'];
+            $page->body = $data['body'];
+
+            if (! empty($page['slug'])) {
+                $page->slug = $data['slug'];
+            }
+
+            $page->save();
+        }
+
+        return redirect()->route('pages.index');
     }
 
     /**
---
+++ resources/views/admin/pages/edit.blade.php
@@ -0,0 +1,73 @@
+@extends('adminlte::page')
+
+@section('title', 'Editar Página')
+
+@section('content_header')
+    <h1>Editar Página</h1>
+@endsection
+
+@section('content')
+
+    @if ($errors->any())
+        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+            <ul>
+                <h5 class="alert-heading"><i class="icon fas fa-ban"></i> Erro(s) ocorrido(s):</h5>
+                <!-- alert-heading -->
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true" class="text-light">&times;</span>
+                <!-- text-light -->
+            </button>
+            <!-- close -->
+        </div>
+        <!-- alert-danger -->
+    @endif
+
+    <div class="card">
+        <div class="card-header">
+            <h5>Preencha os dados abaixo:</h5>
+        </div>
+        <!-- card-header -->
+        <div class="card-body">
+            <form method="POST" action="{{ route('pages.update', ['page' => $page->id]) }}" class="form-horizontal">
+                @method('PUT')
+                @csrf
+                <div class="form-group row">
+                    <label class="col-sm-2 col-form-label">Título</label>
+                    <!-- col-sm-2 -->
+                    <div class="col-sm-10">
+                        <input type="text" name="title" value="{{ $page->title }}"
+                            class="form-control @error('title') is-invalid @enderror" />
+                        <!-- form-control -->
+                    </div>
+                    <!-- col-sm-10 -->
+                </div>
+                <!-- form-group -->
+                <div class="form-group row">
+                    <label class="col-sm-2 col-form-label">Corpo</label>
+                    <!-- col-sm-2 -->
+                    <div class="col-sm-10">
+                        <textarea name="body" class="form-control">{{ $page->body }}</textarea>
+                        <!-- form-control -->
+                    </div>
+                    <!-- col-sm-10 -->
+                </div>
+                <!-- form-group -->
+                <div class="form-group row">
+                    <label class="col-sm-2 col-form-label"></label>
+                    <!-- col-sm-2 -->
+                    <div class="col-sm-10">
+                        <input type="submit" value="Salvar" class="btn btn-success" />
+                    </div>
+                    <!-- col-sm-10 -->
+                </div>
+                <!-- form-group -->
+            </form>
+        </div>
+        <!-- card-body -->
+    </div>
+    <!-- card -->
+@endsection
