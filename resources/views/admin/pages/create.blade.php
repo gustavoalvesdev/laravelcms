792f1dba73c1b332b8f2e0f1f3184e098058df1f
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2021-03-03 14:47:29 -0300

    Página: TinyMCE no Adicionar - Projeto finalizado

--- resources/views/admin/pages/create.blade.php
+++ resources/views/admin/pages/create.blade.php
@@ -49,7 +49,7 @@
                     <label class="col-sm-2 col-form-label">Corpo</label>
                     <!-- col-sm-2 -->
                     <div class="col-sm-10">
-                        <textarea name="body" class="form-control">{{ old('body') }}</textarea>
+                        <textarea name="body" class="form-control bodyfield">{{ old('body') }}</textarea>
                         <!-- form-control -->
                     </div>
                     <!-- col-sm-10 -->
@@ -69,4 +69,30 @@
         <!-- card-body -->
     </div>
     <!-- card -->
+
+    <script src="https://cdn.tiny.cloud/1/b42us2li6se82g05p71vf5ym73octeflt91cvxkuv2yr8c5d/tinymce/5/tinymce.min.js">
+    </script>
+
+    <script>
+        tinymce.init({
+            selector: 'textarea.bodyfield',
+            'height': 300,
+            'menubar': false,
+            'plugins': [
+                'link',
+                'table',
+                'image',
+                'autoresize',
+                'lists'
+            ],
+            'toolbar': 'undo redo | formatselect | bold italic backcolor | alignleft aligncenter alignright alignjustify | table | link image | bullist numlist',
+            'content_css': [
+                "{{ asset('assets/css/content.css') }}"
+            ],
+            'images_upload_url': "{{ route('imageupload') }}",
+            'images_upload_credentials': true,
+            'convert_urls': false
+        });
+
+    </script>
 @endsection
